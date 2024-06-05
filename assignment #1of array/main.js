21f6e5d56ccf0b53d636c97e25dddda45c915373
Musarrat Naz <147928780+Musarratnaz@users.noreply.github.com>
2024-06-05 10:53:46 -0700

    Add files via upload

---
+++ assignment #1of array/main.js
@@ -0,0 +1,52 @@
+//Assignment of Array# 1
+//    studentName =["Musarrat Naz"]
+//    rollNumber= [00432901]                   
+//                     //Q # 1
+//let fruits =["apple","banana","mango"," orange"];
+// console.log("fruit",fruits)
+//                    //Q#2
+//  let numbers=[ 10,20,30,40];
+console.log("numbers");
+//                   // Q#3 
+// let fruitsArray  =["apple","banana","mango"," orange"];
+// let thirdFruit= ["orange"]
+//Q#4
+// let numbers=[ 10,20,30,40];
+// numbers.push(25)
+// console.log(numbers,"numbers" )
+// Q#5
+// let fruits =["apple","banana","mango"," orange"];
+// fruits.push("grape")
+// console.log(fruits, "fruits")
+//Q#6
+// let fruits =["apple","banana","mango"," orange"];
+// let lastFruit=("orange");
+// fruits.pop();
+// console.log(fruits, "lastFruit")
+// Q#7
+//  let fruits =["apple","banana","mango"," orange"];
+//  let firstFruit=("apple")
+//  fruits.shift();
+//  console.log(fruits, "firstFruit")
+//Q#8
+//  let fruits =["apple","banana","mango"," orange"];
+//  fruits.unshift("kiwi")
+//  console.log(fruits, "fruits")
+//Q#9
+// let fruits =["apple","banana","mango"," orange"];
+// fruits.shift();
+// fruits.shift();
+// console.log(fruits, "fruits")
+//respected teacher  Q# 10 ,11 & 12  khud nahikeye hin q k abhi slice &splice abhi nahi ata 
+//                  Q#10
+// let fruits =["apple","banana","mango"," orange"];
+// fruits.splice(1,0,"pineapple","pear");
+// console.log(fruits)
+//Q#11
+//  let fruits =["apple","pineapple","pear","mango"," orange"];                   
+// let citrusFruits =  fruits.slice(0,2);
+// console.log(citrusFruits)
+//Q#12
+// let fruits =["apple","pineapple","pear","mango"," orange"];
+// let  lastTwoFruits=fruits.slice(-2);
+// console.log(lastTwoFruits)
---
+++ assignment #1of array/main.ts
@@ -0,0 +1,67 @@
+               //Assignment of Array# 1
+
+//    studentName =["Musarrat Naz"]
+//    rollNumber= [00432901]                   
+                    
+                    //Question # 1
+ let fruits:string[] =["apple","banana","mango"," orange"];
+
+                   //Question#2
+  let number=[ 10,20,30,40];
+
+
+                  // Question#3 
+let fruitsArray  =["apple","banana","mango"," orange"];
+let thirdFruit= ["orange"]
+
+                      //Question#4
+let numbers:number[]=[ 10,20,30,40];
+numbers.push(25)
+console.log(numbers,"numbers" )
+                        // Question#5
+let fruits:string[] =["apple","banana","mango"," orange"];
+fruits.push("grape")
+console.log(fruits, "fruits")
+
+
+                           //Question#6
+let fruits:string[] =["apple","banana","mango"," orange"];
+let lastFruit=("orange");
+fruits.pop();
+console.log(fruits, "lastFruit")
+
+                           // Question#7
+ let fruits:string[] =["apple","banana","mango"," orange"];
+ let firstFruit=("apple")
+ fruits.shift();
+ console.log(fruits, "firstFruit")
+
+
+                            //Question#8
+ let fruits:string[] =["apple","banana","mango"," orange"];
+ fruits.unshift("kiwi")
+ console.log(fruits, "fruits")
+
+                             //Question#9
+let fruits:string[] =["apple","banana","mango"," orange"];
+fruits.shift();
+fruits.shift();
+console.log(fruits, "fruits")
+
+//                  Question#10
+let fruits:string[] =["apple","banana","mango"," orange"];
+fruits.splice(1,0,"pineapple","pear");
+console.log(fruits)
+
+                    //Question#11
+ let fruits:string[] =["apple","pineapple","pear","mango"," orange"];                   
+let citrusFruits =  fruits.slice(0,2);
+console.log(citrusFruits)
+                    //Question#12
+let fruits:string[] =["apple","pineapple","pear","mango"," orange"];
+ let  lastTwoFruits=fruits.slice(-2);
+console.log(lastTwoFruits)
+
+
+
+
